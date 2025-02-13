081d1292fbc8a1c6f6394e26765344dce90cfd88
Raezard77 <zartisian@gmail.com>
2025-02-13 12:11:57 +0200

    Creating Subdomains Template Generator

--- gulp.js
+++ gulp.js
@@ -1,4 +1,4 @@
-// - Plugins
+//-- Plugins
 import gulp from 'gulp'
 import pug from 'gulp-pug'
 import gulpSass from 'gulp-sass'
@@ -7,17 +7,18 @@ import cleanCSS from 'gulp-clean-css'
 import concat from 'gulp-concat'
 import plumber from 'gulp-plumber'
 import terser from 'gulp-terser'
-import sourcemaps from 'gulp-sourcemaps'
+// import sourcemaps from 'gulp-sourcemaps'
 import imagemin from 'gulp-imagemin'
 import webp from 'gulp-webp'
 import changed from 'gulp-changed'
 import watch from 'gulp-watch'
+import rename from 'gulp-rename'
 import * as dartSass from 'sass'
 const sass = gulpSass(dartSass)
 
 
 
-// - Paths
+//-- Paths
 const pugTree = {
     root: './src/pug/**/*.pug',
     pages: './src/pug/pages/*.pug',
@@ -25,7 +26,7 @@ const pugTree = {
 const scssPath = './src/scss/**/*.scss'
 const jsPath = './src/js/**/*.js'
 const imgPath = './src/img/**/*'
-const jsHTMLFn = ["c", "d", "openURL", "prevQuote", "nextQuote", "goToQuote"]
+const jsHTMLCn = ["c", "d", "openURL", "prevQuote", "nextQuote", "goToQuote", "textSlider"]
 const primJSFile =  [
     './src/js/boilerplate/tools.js',
     './src/js/boilerplate/data.js'
@@ -34,7 +35,12 @@ const watchOptions = {readDelay: 0, ignoreInitial: false}
 
 
 
-// - Build — HTML
+//-- Custom
+const brands = ['lg', 'ua', 'toshiba']
+
+
+
+//-- Build — HTML
 gulp.task('html', async _ => {
     return gulp
         .src(pugTree.pages)
@@ -45,12 +51,12 @@ gulp.task('html', async _ => {
             }
         }))
         .pipe(pug())
-        .pipe(gulp.dest('./dist'))
+        .pipe(gulp.dest('./dist/stager'))
 })
 
 
 
-// - Build — CSS
+//-- Build — CSS
 gulp.task('css', async _ => {
     return gulp
         .src(scssPath)
@@ -62,17 +68,17 @@ gulp.task('css', async _ => {
                 this.emit('end')
             }
         }))
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(sass({ style: 'compressed' }).on('error', sass.logError))
         .pipe(prefix('last 2 versions'))
         .pipe(cleanCSS())
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./dist'))
+        // .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/stager'))
 })
 
 
 
-// - Build — JS
+//-- Build — JS
 gulp.task('js', async _ => {
     return gulp
         .src([
@@ -87,21 +93,21 @@ gulp.task('js', async _ => {
                 this.emit('end')
             }
         }))
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(concat('scripts.js'))
         .pipe(terser({
             mangle: {
                 toplevel: true,
-                reserved: jsHTMLFn
+                reserved: jsHTMLCn
             }
         }))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(gulp.dest('./dist'))
+        // .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./dist/stager'))
 })
 
 
 
-// - Assets - Images Minify
+//-- Assets - Images Minify
 gulp.task('imgmin', async () => {
     return gulp
         .src(imgPath, { encoding: false })
@@ -113,15 +119,31 @@ gulp.task('imgmin', async () => {
 
 
 
-// - Watchers
+//-- Variants
+const createVariantTask = (brand, ext, file, js = false) => () => {
+    const label = js ? file : brand
+    gulp.src(`dist/stager/${label}.${ext}`)
+        .pipe(rename(`${file}.${ext}`))
+        .pipe(gulp.dest(`dist/variants/${brand}`));
+}
+brands.forEach(brand => gulp.task(`${brand}-html`, createVariantTask(brand, 'html', 'index')))
+brands.forEach(brand => gulp.task(`${brand}-css`, createVariantTask(brand, 'css', 'styles')))
+brands.forEach(brand => gulp.task(`${brand}-js`, createVariantTask(brand, 'js', 'scripts', true)))
+
+
+
+//-- Watchers
 gulp.task('watch', () => {
     watch(pugTree.root, watchOptions, gulp.series('html'))
     watch(scssPath, watchOptions, gulp.series('css'))
     watch(jsPath, watchOptions, gulp.series('js'))
     watch(imgPath, watchOptions, gulp.series('imgmin'))
+    brands.forEach(brand => {watch(`dist/stager/${brand}.html`, gulp.series(`${brand}-html`))})
+    brands.forEach(brand => {watch(`dist/stager/${brand}.css`, gulp.series(`${brand}-css`))})
+    brands.forEach(brand => {watch(`dist/stager/scripts.js`, gulp.series(`${brand}-js`))})
 })
 
 
 
-// - Default
+//-- Default
 gulp.task('default', gulp.series('watch'))
--- src/js/boilerplate/data.js
+++ src/js/boilerplate/data.js
@@ -1,4 +1,4 @@
-const textSlider = {
-    wordList: ['ثلاجات', 'غسالات', 'شاشات'],
-    Interval: 3e3  // _ Same as animation duration
-}
+// const textSlider = {
+//     wordList: ['ثلاجات', 'غسالات', 'شاشات'],
+//     Interval: 3e3  // _ Same as animation duration
+// }
